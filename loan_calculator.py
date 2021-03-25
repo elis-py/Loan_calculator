c036d4e6d933d4719b48891d026ba69ba1bb4857
elis-py <efremova.elis@yandex.ru>
2021-03-25 15:28:36 +0300

    added readme.md and a few changes to loan_calculator.py

--- loan_calculator.py
+++ loan_calculator.py
@@ -9,6 +9,7 @@ import argparse
 
 
 def diff_pay(principal, periods, interest):
+    """Computing differential monthly payment"""
     payment_all = 0
     for month in range(1, periods + 1):
         payment = principal / periods + (interest / 1200) * \
@@ -17,21 +18,23 @@ def diff_pay(principal, periods, interest):
         print(f'Month {month}: payment is {math.ceil(payment)}')
     print(f'\nOverpayment = {round(payment_all) - principal}')
 
-def annuity_principal(payment, periods, interest):
+def annuity_principal(payment, periods, interest): 
+    """Computing annuity loan"""
     principal = payment / (((interest / 1200) * (1 + (interest / 1200)) ** periods) / \
-                                     ((1 + (interest / 1200)) ** periods - 1))
+                           ((1 + (interest / 1200)) ** periods - 1))
     print(f'Your loan principal = {math.ceil(principal)}!')
     print(f'Overpayment = {periods * payment - math.ceil(principal)}')
 
-def annuity_payment(principal, periods, interest):
+def annuity_payment(principal, periods, interest): 
+    """Computing annuity payment"""
     payment = principal * (((interest / 1200) * (1 + (interest / 1200)) \
                                  ** periods) / ((1 + (interest / 1200)) ** periods - 1))
     print(f'Your monthly payment = {math.ceil(payment)}!')
     print(f'Overpayment = {periods * math.ceil(payment) - principal}')
 
-def annuity_periods(principal, payment, interest):
-    periods = math.log((payment /(payment - (interest / 1200) \
-                                            * principal)), ((interest / 1200) + 1))
+def annuity_periods(principal, payment, interest): 
+    """Computing periods for annuity loan"""
+    periods = math.log((payment /(payment - (interest / 1200) * principal)), ((interest / 1200) + 1))
     if math.ceil(periods // 12) == 0:
         if math.ceil(periods % 12) == 1:
             print('It will take 1 month to repay this loan!')
@@ -77,12 +80,12 @@ interest = args.interest
 payment = args.payment
 parameters = [principal, periods, interest, payment]
 
-for arg in parameters:  #check if any of the parameters have negative value
+for arg in parameters:  # check if any of the parameters have negative value
     if arg is not None and arg < 0:
         print('Incorrect parameters')
         exit()
 
-if t not in ['diff', 'annuity'] or not interest:  #check the presence of required parameters
+if t not in ['diff', 'annuity'] or not interest:  # check the presence of required parameters
     print('Incorrect parameters')
 elif parameters.count(None) > 1: 
     print('Incorrect parameters')
---
+++ readme.md
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 14:44:06 2021
+
+"""
+
+## Project "Loan calculator"
+
+**Level:** easy.
+
+**Imported libraries:** math, argparse.
+
+
+**Annotation:** The Loan calculator works from the command line and parses 
+the parameters (like a loan principal and interest) according with selected 
+type of loan (annuity payment and differentiated payment). Then it calculates 
+the values the user wants to know (for example, monthly payment or overpayment), 
+and outputs them to the user.
